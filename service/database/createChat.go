39ce131a5a65aa404994373cdfe35e6993ed019b
ricccan <riccardocandidi@gmail.com>
2025-01-17 19:01:27 +0100

    refactor CreateChat function to check for existing conversations before inserting new ones

--- service/database/createChat.go
+++ service/database/createChat.go
@@ -30,7 +30,7 @@ func (db *appdbimpl) CreateChat(nome string, utente int) error { // creazione fu
 		return err
 	}
 
-	query = "INSERT INTO conversations (grup) VALUES ('false')"
+	query = "SELECT conversations.id FROM conversations, us_con as primo, us_con as secondo WHERE conversations.id = primo.conv AND conversations.id = secondo.conv AND primo.us = ? AND secondo.us = ?" // da cambiare
 
 	stmt, err = db.c.Prepare(query) // query
 	if err != nil {
@@ -39,28 +39,60 @@ func (db *appdbimpl) CreateChat(nome string, utente int) error { // creazione fu
 	defer stmt.Close() // Chiude lo statement preparato
 	// Eseguire l'aggiornamento
 
-	result2, err := stmt.Exec()
+	controllo, err := stmt.Query(utente, altro)
 	if err != nil {
 		return err
 	}
 
 	// Controlla il numero di righe interessate
-	rowsAffected, err := result2.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if rowsAffected != 1 {
-		return err
-	}
-
-	chatid, err := result2.LastInsertId()
+	err = controllo.Err()
 	if err != nil {
 		return err
 	}
 
-	db.AddToCollegamento(utente, altro, chatid)
+	var prova *int
+	for controllo.Next() {
+		err = controllo.Scan(&prova)
+		if err != nil {
+			return err
+		}
+	}
 
+	if prova == nil {
+		query = "INSERT INTO conversations (grup) VALUES ('false')"
+
+		stmt, err = db.c.Prepare(query) // query
+		if err != nil {
+			return err // se c è errore
+		}
+		defer stmt.Close() // Chiude lo statement preparato
+		// Eseguire l'aggiornamento
+
+		result2, err := stmt.Exec()
+		if err != nil {
+			return err
+		}
+
+		// Controlla il numero di righe interessate
+		rowsAffected, err := result2.RowsAffected()
+		if err != nil {
+			return err
+		}
+		if rowsAffected != 1 {
+			return err
+		}
+
+		chatid, err := result2.LastInsertId()
+		if err != nil {
+			return err
+		}
+
+		db.AddToCollegamento(utente, altro, chatid)
+
+		return nil
+	}
 	return nil
+
 }
 
 func (db *appdbimpl) AddToCollegamento(utente int, altro int, chat int64) error { // creazione funzione, prende i parametri che ci servono
